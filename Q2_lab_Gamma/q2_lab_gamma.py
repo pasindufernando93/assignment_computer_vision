6cb32ff30b9d8e51b6400b18bfff45e2dd98d220
Pasindu Fernando <pasinduchathuranga12@gmail.com>
2026-04-06 14:23:44 +0530

    Load image and shaped

--- Q2_lab_Gamma/q2_lab_gamma.py
+++ Q2_lab_Gamma/q2_lab_gamma.py
@@ -1,10 +1,13 @@
+
 import cv2
 
 # Load image 
-img_bgr = cv2.imread("/a1images/highlights_and_shadows.jpg")
+img_bgr = cv2.imread("Q2_lab_Gamma/assets/a1images/highlights_and_shadows.jpg")
 img_rgb = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2RGB)   # for matplotlib display
 
 print(f"Image shape : {img_rgb.shape}")
 print(f"Dtype       : {img_rgb.dtype}")
+
+
 
 
